391da04d5d4442a64985e99a31bf9afc3d9947dd
Mohit <mohit24ab@gmail.com>
2026-06-01 04:22:43 +0530

    final changes

--- index.js
+++ index.js
@@ -1,66 +1,73 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
+app.get("/", function (req, res) {
+  fs.readdir("./files", function (err, files) {
+    if (err) {
+      console.log(err);
+      return res.send("Error reading files");
+    }
 
-    fs.readdir('./files', function(err, files) {
-
-        if (err) {
-            console.log(err);
-            return res.send('Error reading files');
-        }
-
-        res.render('index', {
-            files: files
-        });
-
+    res.render("index", {
+      files: files,
     });
-
+  });
 });
 
-app.post('/create', function(req, res) {
+app.post("/create", function (req, res) {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      if (err) throw err;
 
-    fs.writeFile(
-        `./files/${req.body.title.split(' ').join('')}.txt`,
-        req.body.details,
-        function(err) {
-
-            if (err) throw err;
-
-            res.redirect('/');
-        }
-    );
-
+      res.redirect("/");
+    },
+  );
 });
 
-app.get('/file/:filename', function(req, res) {
 
-    fs.readFile(
-        `./files/${req.params.filename}`,
-        'utf-8',
-        function(err, filedata) {
 
-            if (err) throw err;
 
-            res.render('show', {
-                filename: req.params.filename,
-                filedata: filedata
-            });
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      if (err) throw err;
 
-        }
-    );
+      res.render("show", {
+        filename: req.params.filename,
+        filedata: filedata,
+      });
+    },
+  );
+});
 
+
+
+app.get("/edit/:filename", function (req, res) {
+    res.render("edit", {
+        filename: req.params.filename,
+    });
+});
+
+
+app.post('/edit', function(req, res) {
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err) {
+        res.redirect("/");
+    });
 });
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+  console.log("Server running on port 3000");
 });
